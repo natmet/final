40518a1566b55d432b38c732c75312f983b2baaf
natanael alberto <natanaelalbertomed@gmail.com>
2022-04-08 17:11:53 -0400

    proyecto final web design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { RegisterComponent } from './register/register.component';
+import { WorkDetailComponent } from './work-detail/work-detail.component';
+import { WorkComponent } from './work/work.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: 'work', component: WorkComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'about', component: NosotrosComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'work-detail', component: WorkDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { WorkComponent } from './work/work.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FooterComponent } from './footer/footer.component';
+import { WorkDetailComponent } from './work-detail/work-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    WorkComponent,
+    NosotrosComponent,
+    ContactComponent,
+    LoginComponent,
+    RegisterComponent,
+    FooterComponent,
+    WorkDetailComponent
   ],
   imports: [
     BrowserModule,
